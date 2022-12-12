e8f76d4811da8e3cc92126bd09456e1648d4032e
shahinm654 <moradishahin654@gmail.com>
2022-12-12 14:17:54 +0330

    project section from the index move to partial view

---
+++ DevCard_Project/DevCard_Mvc/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Mvc.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
