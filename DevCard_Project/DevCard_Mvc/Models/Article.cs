524f286d22aad490b17f8a0e62d45a0fc0e0a472
shahinm654 <moradishahin654@gmail.com>
2022-12-13 13:39:00 +0330

    header & footer move by partial view

--- DevCard_Project/DevCard_Mvc/Models/Article.cs
+++ DevCard_Project/DevCard_Mvc/Models/Article.cs
@@ -10,5 +10,14 @@ namespace DevCard_Mvc.Models
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Article(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
     }
 }
---
+++ DevCard_Project/DevCard_Mvc/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(id:1, title:"عنوان بهترین مقاله ",description:"خیلی عالی و کامل بود ",image:"blog-post-thumb-card-1.jpg"),
+
+                new Article(id:2, title:"عنوان بهترین مقاله ",description:"خیلی عالی و کامل بود ",image:"blog-post-thumb-card-2.jpg"),
+
+                new Article(id:3, title:"عنوان بهترین مقاله ",description:"خیلی عالی و کامل بود ",image:"blog-post-thumb-card-3.jpg"),
+
+                new Article(id:4, title:"عنوان بهترین مقاله ",description:"خیلی عالی و کامل بود ",image:"blog-post-thumb-card-4.jpg")
+
+
+
+
+            };
+            return View("_LatestArticles",articles);
+        }
+    }
+}
