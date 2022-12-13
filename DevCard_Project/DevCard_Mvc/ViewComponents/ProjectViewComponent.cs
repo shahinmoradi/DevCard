766086bb863d2fee22023614bba6460e5f6a1fad
shahinm654 <moradishahin654@gmail.com>
2022-12-13 09:56:54 +0330

    insert view component  inro index

---
+++ DevCard_Project/DevCard_Mvc/ViewComponents/ProjectViewComponent.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DevCard_Mvc.Models;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(id:1 , name:"پروژه اسنپ", description:"خیلی جامع و کامل بود ",image:"project-1.jpg",client:"shahin"),
+
+                new Project(id:2 , name:"پروژه اسنپ", description:"خیلی جامع و کامل بود ",image:"project-2.jpg",client:"shahin"),
+
+                new Project(id:3 , name:"پروژه اسنپ", description:"خیلی جامع و کامل بود ",image:"project-3.jpg",client:"shahin"),
+
+                new Project(id:3 , name:"پروژه اسنپ", description:"خیلی جامع و کامل بود ",image:"project-4.jpg",client:"shahin"),
+
+            };
+            return View("_Project",projects);
+        }
+    }
+}
